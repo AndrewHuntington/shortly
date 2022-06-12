d57d62eefdab489b4ae2a8edaf8da728d819eb38
Andrew Huntington <andrew.huntington@gmail.com>
2022-06-12 15:53:02 +0900

    Add testing library and jest

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from "next";
+import Navbar from "../components/Navbar";
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Navbar />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
 };
 
 export default Home;
