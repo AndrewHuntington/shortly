f67751f8959a91bf86d7ea6ed259501dcd4f43f3
Andrew Huntington <andrew.huntington@gmail.com>
2022-06-12 14:56:04 +0900

    Setup fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,24 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      primary: {
+        cyan: "hsl(180, 66%, 49%)",
+        darkViolet: "hsl(257, 27%, 26%)",
+      },
+      secondary: {
+        red: "hsl(0, 87%, 67%)",
+      },
+      neutral: {
+        gray: "hsl(0, 0%, 75%)",
+        grayishViolet: "hsl(257, 27%, 63%)",
+        veryDarkBlue: "hsl(255, 11%, 22%)",
+        veryDarkViolet: "hsl(260, 8%, 14%)",
+      },
+    },
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
