f8ae143fe838eb215ef9d6af3f5c8326fc14e9e2
Andrew Huntington <andrew.huntington@gmail.com>
2022-06-12 16:31:40 +0900

    Fix colors in Tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,19 +6,13 @@ module.exports = {
   ],
   theme: {
     colors: {
-      primary: {
-        cyan: "hsl(180, 66%, 49%)",
-        darkViolet: "hsl(257, 27%, 26%)",
-      },
-      secondary: {
-        red: "hsl(0, 87%, 67%)",
-      },
-      neutral: {
-        gray: "hsl(0, 0%, 75%)",
-        grayishViolet: "hsl(257, 27%, 63%)",
-        veryDarkBlue: "hsl(255, 11%, 22%)",
-        veryDarkViolet: "hsl(260, 8%, 14%)",
-      },
+      primaryCyan: "hsl(180, 66%, 49%)",
+      primaryDarkViolet: "hsl(257, 27%, 26%)",
+      secondaryRed: "hsl(0, 87%, 67%)",
+      neutralGray: "hsl(0, 0%, 75%)",
+      neutralGrayishViolet: "hsl(257, 27%, 63%)",
+      neutralVeryDarkBlue: "hsl(255, 11%, 22%)",
+      neutralVeryDarkViolet: "hsl(260, 8%, 14%)",
     },
     fontFamily: {
       sans: ["Poppins", "sans-serif"],
