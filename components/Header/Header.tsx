f372bab5c9821931e4e3ae00ffa67a501ee87079
Andrew Huntington <andrew.huntington@gmail.com>
2022-06-13 10:06:32 +0900

    Organize image imports/exports into one file

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-import logo from "../../public/images/logo.svg";
-import hamburger from "../../public/images/hamburger.png";
+import { logo, hamburger } from "../../public/images/";
 import Image from "next/image";
 import Link from "next/link";
 
